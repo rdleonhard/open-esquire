d31edcdde0d5abf2c8275a377248ece674e6cec0
Robert Leonhard <rdleonhard@protonmail.com>
2025-04-06 12:09:04 -0400

    fix: update favicon path to correct location in images directory

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="/images/favicon.ico" />
       </head>
       <body>{children}</body>
     </html>
